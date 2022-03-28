884799ef20ae8961cbf7859b909c5cf1e1b70fdf
Ramon Sabater Roca <ramon.sabater@students.salle.url.edu>
2022-03-28 18:27:18 +0200

    Added math grammar to the file

--- src/tokens/CCodeToken.java
+++ src/tokens/CCodeToken.java
@@ -1,4 +1,11 @@
 package tokens;
 
+import java.util.ArrayList;
+
 public class CCodeToken extends Token{
+    private ArrayList<Token> code;
+
+    public CCodeToken() {
+        super.name = "ccode";
+    }
 }
